7690c55a6cf99a405a0e9a344d78f1c71cdb1fea
lundjrl <jrlbidamin@gmail.com>
2019-08-19 17:49:37 -0400

    Added aliens and configured bullets for game.

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,4 +1,5 @@
 from settings import Settings
+from game_stats import GameStats
 from ship import Ship
 from pygame.sprite import Group
 import pygame
@@ -13,19 +14,28 @@ def run_game():
             (ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Zenthos Elite")
     
-    # Rendering ship
-    ship = Ship(ai_settings, screen)
+    # Create an instance to store gaem stats.
+    stats = GameStats(ai_settings)
 
-    # Grouping for bullets
+    # Rendering objects
+    ship = Ship(ai_settings, screen)
     bullets = Group()
+    swarm = Group()
+
+    # Creating the swarm
+    gf.create_fleet(ai_settings, screen, ship, swarm)
 
     # Loopback for the game
     while True:
         # Watch for keyboard input
         gf.check_events(ai_settings, screen, ship, bullets)
-        ship.update()
-        gf.update_bullets(bullets)
-        gf.update_screen(ai_settings, screen, ship, bullets)
+        
+        if stats.game_active:
+            ship.update()
+            gf.update_bullets(ai_settings, screen, ship, swarm, bullets)
+            gf.update_aliens(ai_settings, stats, screen, ship, swarm, bullets)
+
+        gf.update_screen(ai_settings, screen, ship, swarm, bullets)
 
         # Redraw the screen
         screen.fill(ai_settings.bg_color)
--- settings.py
+++ settings.py
@@ -10,10 +10,16 @@ class Settings():
 
         # Ship settings
         self.ship_speed_factor = 1.5
+        self.ship_lives = 3
 
         # Bullet settings
-        self.bullet_speed_factor = 3
-        self.bullet_width = 4
+        self.bullet_speed_factor = 4
+        self.bullet_width = 300
         self.bullet_height = 15
         self.bullet_color = 20, 60, 60
         self.bullets_on_screen = 5
+
+        # Alien settings
+        self.alien_speed_factor = 1
+        self.fleet_drop_speed = 10
+        self.fleet_direction = 1
